83e08dbe68a9b941c8e6f6bc61ee3ba35f1daab5
Kris Wallsmith <kris.wallsmith@gmail.com>
2010-03-10 12:43:35 -0800

    Added code for unregistering the autoload-again mechanism, if you so choose.

--- project/lib/config/BaseApplicationConfiguration.class.php
+++ project/lib/config/BaseApplicationConfiguration.class.php
@@ -2,4 +2,14 @@
 
 class BaseApplicationConfiguration extends sfApplicationConfiguration
 {
+  /**
+   * @see sfApplicationConfiguration
+   */
+  public function initConfiguration()
+  {
+    parent::initConfiguration();
+
+    // speed up the dev environment
+    //sfAutoloadAgain::getInstance()->unregister();
+  }
 }
